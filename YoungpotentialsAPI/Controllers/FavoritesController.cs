d7bf5380927fc8c39e4d51711f52ae69fb4801c9
cherrymcgerry <robbert.coucke@hotmail.be>
2020-02-11 14:58:03 +0100

    added favorites and offer controller

---
+++ YoungpotentialsAPI/Controllers/FavoritesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace YoungpotentialsAPI.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class FavoritesController : Controller
+    {
+
+        private IFavoritesService _favoritesService;
+
+        public FavoritesController(IFavoritesService favoritesService)
+        {
+            _favoritesService = favoritesService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ YoungpotentialsAPI/Controllers/OfferController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace YoungpotentialsAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OfferController : Controller
+    {
+
+        private IOfferService _offerService;
+
+        public OfferController(IOfferService offerService)
+        {
+            _offerService = offerService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
