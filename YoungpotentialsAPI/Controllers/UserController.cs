8dcc70445daad10d24e85b8c677693a6e7050f4d
cherrymcgerry <robbert.coucke@hotmail.be>
2020-02-11 14:28:50 +0100

    fixed appsettings

--- YoungpotentialsAPI/Controllers/UserController.cs
+++ YoungpotentialsAPI/Controllers/UserController.cs
@@ -12,118 +12,121 @@ using System.Security.Claims;
 using YoungpotentialsAPI.Models.Responses;
 using YoungpotentialsAPI.Models.Requests;
 using Youngpotentials.Domain.testEntities;
+using Youngpotentials.Service;
+using AutoMapper;
+using YoungpotentialsAPI.Helpers;
 
 namespace YoungpotentialsAPI.Controllers
 {
 
-    //[Authorize]
-    //[ApiController]
-    //[Route("[controller]")]
-    //public class UserController : Controller
-    //{
-    //    private IUserService _userService;
-    //    private readonly AppSettings _appSettings;
-    //    private IMapper _mapper;
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        private IUserService _userService;
+        private readonly AppSettings _appSettings;
+        private IMapper _mapper;
 
-    //    public UsersController(IUserService userService,IMapper mapper, IOptions<AppSettings> appSettings)
-    //    {
-    //        _userService = userService;
-    //        _appSettings = appSettings;
-    //        _mapper = mapper;
-    //    }
+        public UserController(IUserService userService, IMapper mapper, IOptions<AppSettings> appSettings)
+        {
+            _userService = userService;
+            _appSettings = appSettings.Value;
+            _mapper = mapper;
+        }
 
-    //    [AllowAnonymous]
-    //    [HttpPost("authenticate")]
-    //    public IActionResult Authenticate([FromBody]AuthenticateModel model)
-    //    {
-    //        var user = _userService.Authenticate(model.Email, model.Password);
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody]AuthenticateModel model)
+        {
+            var user = _userService.Authenticate(model.Email, model.Password);
 
-    //        if (user == null)
-    //        {
-    //            return BadRequest(new { message = "email or password is incorrect" });
-    //        }
+            if (user == null)
+            {
+                return BadRequest(new { message = "email or password is incorrect" });
+            }
 
-    //        var tokenHandler = new JwtSecurityTokenHandler();
-    //        var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
-    //        var tokenDescriptor = new SecurityTokenDescriptor
-    //        {
-    //            Subject = new System.Security.Claims.ClaimsIdentity(new Claim[] {
-    //                new Claim(ClaimTypes.Name, user.Id.ToString())
-    //            }),
-    //            Expires = DateTime.UtcNow.AddDays(7),
-    //            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-    //        };
-    //        var token = tokenHandler.CreateToken(tokenDescriptor);
-    //        var tokenString = tokenHandler.WriteToken(token);
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new System.Security.Claims.ClaimsIdentity(new Claim[] {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
 
-    //        return Ok(new
-    //        {
-    //            Id = user.Id,
-    //            Email = user.Email,
-    //            FirstName = user.FirstName,
-    //            LastName = user.LastName,
-    //            Token = tokenString
-    //        });
+            return Ok(new
+            {
+                Id = user.Id,
+                Email = user.Email,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Token = tokenString
+            });
 
-    //    }
+        }
 
-    //    [AllowAnonymous]
-    //    [HttpPost("register")]
-    //    public IActionResult Register([FromBody]UserRegistrationRequest model)
-    //    {
-    //        var user = _mapper.Map<AspNetUser>(model);
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public IActionResult Register([FromBody]UserRegistrationRequest model)
+        {
+            var user = _mapper.Map<AspNetUser>(model);
 
-    //        try
-    //        {
-    //            _userService.Create(user, model.Password);
-    //            return Ok();
-    //        }
-    //        catch(Exception e)
-    //        {
-    //            return BadRequest(new { message = e.Message });
-    //        }
+            try
+            {
+                _userService.Create(user, model.Password);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { message = e.Message });
+            }
 
-    //    }
+        }
 
-    //    [HttpGet]
-    //    public IActionResult GetAll()
-    //    {
-    //        var users = _userService.GetAll();
-    //        var model = _mapper.Map<IList<UserResponse>>(users);
-    //        return Ok(model);
-    //    }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _userService.GetAll();
+            var model = _mapper.Map<IList<UserResponse>>(users);
+            return Ok(model);
+        }
 
-    //    [HttpGet("{id}")]
-    //    public IActionResult GetById(int id)
-    //    {
-    //        var user = _userService.GetById(id);
-    //        var model = _mapper.Map<UserResponse>(user);
-    //        return Ok(model);
-    //    }
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var user = _userService.GetById(id);
+            var model = _mapper.Map<UserResponse>(user);
+            return Ok(model);
+        }
 
-    //    [HttpPut("{id}")]
-    //    public IActionResult Update(int id, [FromBody]UserUpdateRequest model)
-    //    {
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody]UserUpdateRequest model)
+        {
 
-    //        var user = _mapper.Map<AspNetUser>(model);
-    //        user.Id = id;
-    //        try
-    //        {
-    //            _userService.Update(user, model.Password);
-    //            return Ok();
-    //        }
-    //        catch(Exception e)
-    //        {
-    //            return BadRequest(new { message = e.Message });
-    //        }
-    //    }
+            var user = _mapper.Map<AspNetUser>(model);
+            user.Id = id;
+            try
+            {
+                _userService.Update(user, model.Password);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { message = e.Message });
+            }
+        }
 
-    //    [HttpDelete("{id}")]
-    //    public IActionResult Delete(int id)
-    //    {
-    //        _userService.Delete(id);
-    //        return Ok();
-    //    }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _userService.Delete(id);
+            return Ok();
+        }
 
-    //}
+    }
 }
