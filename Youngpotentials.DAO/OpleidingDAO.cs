11589eafb3b13a7bff7e7afa65c84acd99b28b71
Ibrahim Hag Kasem <r0707581@student.vives.be>
2020-02-17 10:29:55 +0100

    studiegebiedDAO afwerken

---
+++ Youngpotentials.DAO/OpleidingDAO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Youngpotentials.Domain.Entities;
+
+namespace Youngpotentials.DAO
+{
+    public interface IOpleidingDAO
+    {
+        IEnumerable<Opleiding> GetAll();
+        Opleiding GetById(string id);
+    }
+    public class OpleidingDAO
+    {
+    }
+}
---
+++ Youngpotentials.DAO/StudiegebiedDAO.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Youngpotentials.Domain.Entities;
+
+namespace Youngpotentials.DAO
+{
+    public interface IStudiegebiedDAO
+    {
+        IEnumerable<Studiegebied> GetAll();
+        Studiegebied GetById(string id);
+        Studiegebied CreateStudiegebied(Studiegebied studiegebied);
+        void UpdateStudegebied(Studiegebied studiegebied);
+        void DeleteStudieGebied(string id);
+    }
+    public class StudiegebiedDAO : IStudiegebiedDAO
+    {
+        private YoungpotentialsContext _db;
+
+        public StudiegebiedDAO()
+        {
+            _db = new YoungpotentialsContext();
+        }
+
+        public Studiegebied CreateStudiegebied(Studiegebied studiegebied)
+        {
+            _db.Entry(studiegebied).State = EntityState.Added;
+            _db.SaveChanges();
+            return studiegebied;
+        }
+
+        public void DeleteStudieGebied(string id)
+        {
+            var studiegebied = _db.Studiegebied.FirstOrDefault(s => s.Id == id);
+            _db.Studiegebied.Remove(studiegebied);
+            _db.SaveChanges();
+        }
+
+        public IEnumerable<Studiegebied> GetAll()
+        {
+            return _db.Studiegebied.Include(o => o.Opleiding)
+                                   .Include(f => f.StudiegebiedOffer).ToList();
+        }
+
+        public Studiegebied GetById(string id)
+        {
+            return _db.Studiegebied.Where(s => s.Id == id)
+                                   .Include(o => o.Opleiding)
+                                   .Include(f => f.StudiegebiedOffer).FirstOrDefault();
+        }
+
+        public void UpdateStudegebied(Studiegebied studiegebied)
+        {
+            _db.Entry(studiegebied).State = EntityState.Modified;
+            _db.SaveChanges();
+        }
+    }
+}
