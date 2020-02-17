e06bd6efc2a8babbcb944444e25e0643b580d39c
cherrymcgerry <robbert.coucke@hotmail.be>
2020-02-17 10:13:59 +0100

    added roleService

---
+++ Youngpotentials.Service/RoleService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Youngpotentials.DAO;
+using Youngpotentials.Domain.Entities;
+
+namespace Youngpotentials.Service
+{
+    public interface IRoleService
+    {
+        IEnumerable<AspNetRoles> GetAllRoles();
+        AspNetRoles GetRoleById(int id);
+    }
+    public class RoleService : IRoleService
+    {
+
+        private IRoleDAO _roleDAO;
+
+        public RoleService(IRoleDAO roleDAO)
+        {
+            _roleDAO = roleDAO;
+        }
+
+        public IEnumerable<AspNetRoles> GetAllRoles()
+        {
+            return _roleDAO.GetAllRoles();
+        }
+
+        public AspNetRoles GetRoleById(int id)
+        {
+            return _roleDAO.GetRoleById(id);
+        }
+    }
+}
