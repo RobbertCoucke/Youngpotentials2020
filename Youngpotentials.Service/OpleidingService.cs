dcba9b45c9dc1aec7ca3331c0215004a7437c9e1
Ibrahim Hag Kasem <r0707581@student.vives.be>
2020-02-17 12:05:55 +0100

    OpleidingService afwerken

---
+++ Youngpotentials.Service/OpleidingService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Youngpotentials.DAO;
+using Youngpotentials.Domain.Entities;
+
+namespace Youngpotentials.Service
+{
+    public interface IOpleidingService
+    {
+        IEnumerable<Opleiding> GetAll();
+        Opleiding GetById(string id);
+        IEnumerable<Opleiding> GetAllByStudiegebied(string id);
+        void DeleteById(string id);
+        void Update(Opleiding opleiding);
+        Opleiding CreateOpleiding(Opleiding opleiding);
+    }
+    public class OpleidingService : IOpleidingService
+    {
+        private IOpleidingDAO _opleidingDAO;
+
+        public OpleidingService(IOpleidingDAO opleidingDAO)
+        {
+            _opleidingDAO = opleidingDAO;
+        }
+        public Opleiding CreateOpleiding(Opleiding opleiding)
+        {
+            return _opleidingDAO.CreateOpleiding(opleiding);
+        }
+
+        public void DeleteById(string id)
+        {
+            _opleidingDAO.DeleteById(id);
+        }
+
+        public IEnumerable<Opleiding> GetAll()
+        {
+            return _opleidingDAO.GetAll();
+        }
+
+        public IEnumerable<Opleiding> GetAllByStudiegebied(string id)
+        {
+            return _opleidingDAO.GetAllByStudiegebied(id);
+        }
+
+        public Opleiding GetById(string id)
+        {
+            return _opleidingDAO.GetById(id);
+        }
+
+        public void Update(Opleiding opleiding)
+        {
+            _opleidingDAO.Update(opleiding);
+        }
+    }
+}
