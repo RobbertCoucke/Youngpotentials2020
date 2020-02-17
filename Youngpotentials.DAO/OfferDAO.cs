11c30994d47cd2fdc43c7207adccc8545985b5eb
cherrymcgerry <robbert.coucke@hotmail.be>
2020-02-17 15:41:50 +0100

    added offerDAO

---
+++ Youngpotentials.DAO/OfferDAO.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Youngpotentials.Domain.Entities;
+
+namespace Youngpotentials.DAO
+{
+    public interface IOfferDAO
+    {
+        IEnumerable<Offers> GetAllOffers();
+        IEnumerable<Offers> GetAllOffersByCompany(int id);
+        Offers GetOffersById(int id);
+        Offers CreateOffer(Offers offer);
+        void UpdateOffer(Offers offer);
+        void DeleteOffer(int id);
+    }
+    public class OfferDAO : IOfferDAO
+    {
+
+        private YoungpotentialsContext _db;
+
+        public OfferDAO()
+        {
+            _db = new YoungpotentialsContext();
+        }
+
+        public Offers CreateOffer(Offers offer)
+        {
+            _db.Entry(offer).State = Microsoft.EntityFrameworkCore.EntityState.Added;
+            _db.SaveChanges();
+            return offer;
+        }
+
+        public void DeleteOffer(int id)
+        {
+            var offer = _db.Offers.Where(o => o.Id == id).FirstOrDefault();
+            _db.Offers.Remove(offer);
+            _db.SaveChanges();
+        }
+
+        public IEnumerable<Offers> GetAllOffers()
+        {
+            return _db.Offers.ToList();
+        }
+
+        public IEnumerable<Offers> GetAllOffersByCompany(int id)
+        {
+            return _db.Offers.Where(o => o.CompanyId == id).ToList();
+        }
+
+        public Offers GetOffersById(int id)
+        {
+            return _db.Offers.Where(o => o.Id == id).FirstOrDefault();
+        }
+
+        public void UpdateOffer(Offers offer)
+        {
+            _db.Entry(offer).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _db.SaveChanges();
+        }
+    }
+}
---
+++ Youngpotentials.Service/OfferService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Youngpotentials.DAO;
+using Youngpotentials.Domain.Entities;
+
+namespace Youngpotentials.Service
+{
+    public interface IOfferService
+    {
+        IEnumerable<Offers> GetAllOffers();
+        IEnumerable<Offers> GetAllOffersByCompany(int id);
+        Offers GetOffersById(int id);
+        Offers CreateOffer(Offers offer);
+        void UpdateOffer(Offers offer);
+        void DeleteOffer(int id);
+        IEnumerable<Offers> GetOffersByTagId(string id);
+        IEnumerable<Offers> GetOffersByTags(IList<string> ids);
+        IEnumerable<Offers> GetOffersByTypes(IList<string> types);
+        IEnumerable<Offers> GetOffersByTypesAndTags(IList<string> ids, IList<string> types);
+    }
+    public class OfferService : IOfferService
+    {
+
+        private IOfferDAO _offerDAO;
+        
+        public OfferService(IOfferDAO offerDAO)
+        {
+            _offerDAO = offerDAO;
+        }
+        public Offers CreateOffer(Offers offer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteOffer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Offers> GetAllOffers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Offers> GetAllOffersByCompany(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Offers GetOffersById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Offers> GetOffersByTagId(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Offers> GetOffersByTags(IList<string> ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Offers> GetOffersByTypes(IList<string> types)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Offers> GetOffersByTypesAndTags(IList<string> ids, IList<string> types)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateOffer(Offers offer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
