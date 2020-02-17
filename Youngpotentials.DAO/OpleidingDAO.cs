77818ca836556927cd79a3cd95a8aed56382b0d1
Ibrahim Hag Kasem <r0707581@student.vives.be>
2020-02-17 10:47:08 +0100

    opleidingDAO afwerken

--- Youngpotentials.DAO/OpleidingDAO.cs
+++ Youngpotentials.DAO/OpleidingDAO.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Youngpotentials.Domain.Entities;
 
@@ -9,8 +11,59 @@ namespace Youngpotentials.DAO
     {
         IEnumerable<Opleiding> GetAll();
         Opleiding GetById(string id);
+        IEnumerable<Opleiding> GetByStudiegebied(string id);
+        void DeleteById(string id);
+        void Update(Opleiding opleiding);
+        Opleiding CreateOpleiding(Opleiding opleiding);
     }
-    public class OpleidingDAO
+    public class OpleidingDAO : IOpleidingDAO
     {
+        private YoungpotentialsContext _db;
+
+        public OpleidingDAO()
+        {
+            _db = new YoungpotentialsContext();
+        }
+        public Opleiding CreateOpleiding(Opleiding opleiding)
+        {
+            _db.Entry(opleiding).State = EntityState.Added;
+            _db.SaveChanges();
+            return opleiding;
+        }
+
+        public void DeleteById(string id)
+        {
+            var opleiding = _db.Opleiding.FirstOrDefault(o => o.Id == id);
+            _db.Opleiding.Remove(opleiding);
+            _db.SaveChanges();
+        }
+
+        public IEnumerable<Opleiding> GetAll()
+        {
+            return _db.Opleiding.Include(s => s.IdStudiegebiedNavigation)
+                                .Include(a => a.Afstudeerrichting)
+                                .Include(o => o.OpleidingOffer)
+                                .ToList();
+        }
+
+        public Opleiding GetById(string id)
+        {
+            return _db.Opleiding.Where(op => op.Id == id).Include(s => s.IdStudiegebiedNavigation)
+                                .Include(a => a.Afstudeerrichting)
+                                .Include(o => o.OpleidingOffer).FirstOrDefault();
+        }
+
+        public IEnumerable<Opleiding> GetByStudiegebied(string id)
+        {
+            return _db.Opleiding.Where(op => op.IdStudiegebied == id).Include(s => s.IdStudiegebiedNavigation)
+                    .Include(a => a.Afstudeerrichting)
+                    .Include(o => o.OpleidingOffer).ToList();
+        }
+
+        public void Update(Opleiding opleiding)
+        {
+            _db.Entry(opleiding).State = EntityState.Modified;
+            _db.SaveChanges();
+        }
     }
 }
