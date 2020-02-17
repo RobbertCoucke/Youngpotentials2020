a59363dff8f5a4f0db7230b381f1195a4a37726a
Ibrahim Hag Kasem <r0707581@student.vives.be>
2020-02-17 11:59:29 +0100

    studiegebiedService afwerken

---
+++ Youngpotentials.Service/StudiegebiedService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Youngpotentials.DAO;
+using Youngpotentials.Domain.Entities;
+
+namespace Youngpotentials.Service
+{
+    public interface IStuidegebiedService
+    {
+        IEnumerable<Studiegebied> GetAll();
+        Studiegebied GetById(string id);
+        Studiegebied CreateStudiegebied(Studiegebied studiegebied);
+        void UpdateStudegebied(Studiegebied studiegebied);
+        void DeleteStudieGebied(string id);
+    }
+    public class StudiegebiedService : IStuidegebiedService
+    {
+        private IStudiegebiedDAO _studiegebiedDAO;
+
+        public StudiegebiedService(IStudiegebiedDAO studiegebiedDAO)
+        {
+            _studiegebiedDAO = studiegebiedDAO;
+        }
+        public Studiegebied CreateStudiegebied(Studiegebied studiegebied)
+        {
+            return _studiegebiedDAO.CreateStudiegebied(studiegebied);
+        }
+
+        public void DeleteStudieGebied(string id)
+        {
+            _studiegebiedDAO.DeleteStudieGebied(id);
+        }
+
+        public IEnumerable<Studiegebied> GetAll()
+        {
+            return _studiegebiedDAO.GetAll();
+        }
+
+        public Studiegebied GetById(string id)
+        {
+            return _studiegebiedDAO.GetById(id);
+        }
+
+        public void UpdateStudegebied(Studiegebied studiegebied)
+        {
+            _studiegebiedDAO.UpdateStudegebied(studiegebied);
+        }
+    }
+}
