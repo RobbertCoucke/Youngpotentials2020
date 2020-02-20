a8591e009bb56f6dc15d3ded469a2317652df8a8
cherrymcgerry <robbert.coucke@hotmail.be>
2020-02-20 14:05:17 +0100

    upload

--- YoungpotentialsAPI/Startup.cs
+++ YoungpotentialsAPI/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,10 +8,12 @@ using AutoMapper;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
@@ -115,6 +118,10 @@ namespace YoungpotentialsAPI
             {
                 endpoints.MapControllers();
             });
+
+            app.UseStaticFiles();
+         
+
         }
     }
 }
